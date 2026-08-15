26a966d5ad5987c59545f509035ea8c504a59878
leemageorge555 <leemageorge555@gmail.com>
2026-08-16 05:11:52 +0530

    hero banner speed updated

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,6 +1,7 @@
 'use client'
 import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
+
 import HeroBanner from "../../assets/backgroundBanner2.webp"
 import HeroBanner1 from "../../assets/backgroundBanner6.webp"
 import HeroBanner2 from "../../assets/backgroundBanner4.webp"
@@ -11,16 +12,19 @@ import "swiper/css";
 const HeroSection = () => {
   return (
     <Swiper
+
+
     modules={[Autoplay]}
     slidesPerView={1}
     loop={true}
     autoplay={{
-      delay:3000,
+      delay:1500,
       disableOnInteraction: false
     }}
+    speed={1000}
      className=" ">
       <SwiperSlide>
-        <div className="relative w-full aspect-[1920/800] ">
+        <div className="relative w-full aspect-[1920/800] transition-all duration-500 ">
            <Image src={HeroBanner} alt="Hero Banner" className="object-cover " />
         </div>
       </SwiperSlide>
